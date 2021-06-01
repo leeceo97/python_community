5358dda7e35b0f19d10720cc7a1827590eeb24fd
LeeHyeongJune <leeceo97@naver.com>
2021-06-01 23:21:39 +0900

    (feat)배포 완료

--- community/settings.py
+++ community/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mfeej$c41f00hvmkdxvf$7mwwzr-&2*cm#x7qjb9eh_g#5^8*j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
